9ba907c0b30935f3f77fbd6acd90d25175f57c45
François Mockers <francois.mockers@vleue.com>
2025-07-04 23:52:39 +0200

    start

---
+++ src/binary_size.rs
@@ -0,0 +1,38 @@
+use std::{collections::HashMap, path::Path};
+
+use xshell::{Shell, cmd};
+
+use crate::Metrics;
+
+pub struct BinarySize {
+    pub example_name: String,
+}
+
+impl BinarySize {
+    pub fn on(example_name: String) -> Self {
+        Self {
+            example_name: if example_name == "" {
+                "breakout".to_string()
+            } else {
+                example_name
+            },
+        }
+    }
+}
+
+impl Metrics for BinarySize {
+    fn prepare(&self) {
+        let example = &self.example_name;
+        let sh = Shell::new().unwrap();
+        cmd!(sh, "cargo build --release --example {example}")
+            .run()
+            .unwrap();
+    }
+
+    fn collect(&self) -> HashMap<String, u64> {
+        let target_dir = Path::new("target/release/examples");
+        let file_path = target_dir.join(&self.example_name);
+        let size = file_path.metadata().unwrap().len();
+        HashMap::from([("native.size".to_string(), size)])
+    }
+}
